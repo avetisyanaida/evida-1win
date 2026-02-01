d9751d16fb5dcb1dce772c0bd044b77df0b74334
Aida <aida.avetisyan93@gmail.com>
2026-02-01 19:48:51 +0400

    change for png in svg

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -185,7 +185,7 @@ export const Header = ({
             <div className="main-block-item">
                 <button className="logo-profile" onClick={handleMenuHover}>
                     <Image
-                        src={!isMobile ? "/logo2.png" : "/logo.png"}
+                        src={!isMobile ? "/evida-text.svg" : "/evida-text.svg"}
                         alt="logo"
                         width={100}
                         height={100}
