b58f0cdc0df259fa2058e92c9fdf3764472e7b50
Aida <aida.avetisyan93@gmail.com>
2026-02-04 20:29:08 +0400

    add min-height in platform

--- src/components/IGaming-Platform/Platform.tsx
+++ src/components/IGaming-Platform/Platform.tsx
@@ -1,7 +1,7 @@
 export const Platform = () => {
     return <section>
         <div className="container">
-            <div style={{backgroundColor:'red', display:'flex', flexDirection: 'column', gap: '10px', padding: '10px'}}>
+            <div style={{backgroundColor:'red', display:'flex', flexDirection: 'column', gap: '10px', padding: '10px', minHeight: '56px'}}>
                 <p style={{color: 'white'}}>  Built as a <strong>technical demo of a casino platform</strong> for iGaming startups and affiliates.{" "}</p>
                 <a style={{color: 'white'}} href="/igaming-platform">View platform details</a>
             </div>
